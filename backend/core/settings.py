b87575f46f3f5b55d19bb9a63f37f5c35b75e103
alenz <alenzjaison@gmail.com>
2026-08-17 11:14:20 +0530

    Fix vercel.json location and python entrypoint

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/6.1/how/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
